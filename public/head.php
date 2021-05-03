5e8134240cf568855c6f49bc727a6a0e9222efcf
Thomas <42217466+ThomasLEBRET@users.noreply.github.com>
2021-05-03 09:41:04 +0200

    intégration linkedin et github footer

--- public/head.php
+++ public/head.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="public/css/main.css">
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script type="text/javascript" src="https://platform.linkedin.com/badges/js/profile.js" async defer></script>
     <title>Portfolio-Thomas-LEBRET</title>
   </head>
   <body>
