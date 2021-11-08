3e57a9c304b963ad0106b633d925bdfce1bf958c
Simon Belhassen <simonbelhassen4@gmail.com>
2021-11-08 09:17:50 +0100

    avancé du projet

--- views/competences.php
+++ views/competences.php
@@ -5,116 +5,110 @@
 
 <div class="container">
     <div class="block">
-        <h2>Langage</h2>
+        <h2><u>Langage</u></h2>
         <br>
         <div class="competences">
-            <h3>Front End : Html 5, CSS 3</h3>
-            <div class="border">
+            <h3><u>Front End</u> : Html 5, CSS 3</h3>
+           <!--   <div class="border">
                 <div class="content" style="width: 90%"></div>
-            </div>
+            </div>-->
         </div>
 
         <div class="competences">
-            <h3>Back End : PHP, SQL,  XML</h3>
-            <div class="border">
+            <h3><u>Back End</u> : PHP, SQL,  XML</h3>
+           <!--  <div class="border">
                 <div class="content" style="width: 90%"></div>
-            </div>
+            </div> -->
         </div>
 
         <div class="competences">
             <h3>Java, Python</h3>
-            <div class="border">
+          <!--   <div class="border">
                 <div class="content" style="width: 50%"></div>
-            </div>
+            </div>--> 
         </div>
 
         <div class="competences">
             <h3>Ajax</h3>
-            <div class="border">
+           <!--   <div class="border">
                 <div class="content" style="width: 80%"></div>
-            </div>
+            </div>-->
         </div>
 
     </div>
 
     <div class="block">
-        <h2>Framework</h2>
+        <h2><u>Framework</u></h2>
         <br>
         <div class="competences">
-            <h3>CSS : Bootstrap</h3>
-            <div class="border">
+            <h3><u>CSS</u> : Bootstrap</h3>
+           <!--   <div class="border">
                 <div class="content" style="width: 70%"></div>
-            </div>
+            </div>-->
         </div>
 
         <div class="competences">
-            <h3>PHP : Symfony , Laravel</h3>
-            <div class="border">
+            <h3><u>PHP</u> : Symfony , Laravel</h3>
+          <!--    <div class="border">
                 <div class="content" style="width: 80%"></div>
-            </div>
+            </div>-->
         </div>
 
         <div class="competences">
-            <h3>Python : Django</h3>
-            <div class="border">
+            <h3><u>Python</u> : Django</h3>
+           <!--   <div class="border">
                 <div class="content" style="width: 60%"></div>
-            </div>
+            </div>-->
         </div>
     </div>
 
     <div class="block">
-        <h2>Environnement</h2>
+        <h2><u>Environnement</u></h2>
         <br>
         <div class="competences">
             <h3>Git, GitHub</h3>
-            <div class="border">
+          <!--   <div class="border">
                 <div class="content" style="width: 70%"></div>
-            </div>
+            </div>--> 
         </div>
 
         <div class="competences">
-            <h3>Bases de données : MySQL, <br> phpMyAdmin, <br>
+            <h3><u>Bases de données</u> : MySQL, <br> phpMyAdmin, <br>
             MCD / MLD</h3>
-            <div class="border">
+           <!--  <div class="border">
                 <div class="content" style="width: 80%"></div>
-            </div>
+            </div>--> 
         </div>
 
         <div class="competences">
             <h3>Linux</h3>
-            <div class="border">
+         <!--     <div class="border">
                 <div class="content" style="width: 80%"></div>
-            </div>
+            </div>-->
         </div>
 
-        <div class="competences">
-            <h3>Docker</h3>
-            <div class="border">
-                <div class="content" style="width: 50%"></div>
-            </div>
-        </div>
     </div>
 
     <div class="block">
-        <h2>Outils bureautique</h2>
+        <h2> <u> Outils bureautique</u></h2>
         <br>
         <div class="competences">
-            <h3>IDE : Visual Studio Code, <br> SublimText, NetBean</h3>
-            <div class="border">
+            <h3><u>IDE</u> : Visual Studio Code, <br> SublimText, NetBean</h3>
+          <!--   <div class="border">
                 <div class="content" style="width: 90%"></div>
-            </div>
+            </div> -->
         </div>
 
         <div class="competences">
             <h3>Suite Office</h3>
-            <div class="border">
+           <!--   <div class="border">
                 <div class="content" style="width: 60%"></div>
-            </div>
+            </div>-->
         </div>
     </div>
 </div>
-
-<h2>Diplômes et certifications</h2>
+<br><br>
+<h2><u> Diplômes et certifications</u></h2>
 <div class="containerDipExp" style="height: 200px;
     display: flex;
     align-items: center;
--- views/experiences-pro.php
+++ views/experiences-pro.php
@@ -1,34 +1,36 @@
 <div id="experiences-pro">
     <h1 style="text-transform: uppercase;">Experiences professionnelles</h1>
     <hr class="separator">
-    <div class="container">
+  <div class="container">
     <div class="experience">
-        <img rel="CEA" src="public/img/cea.png">
-        <a href="#" target="_blank">
-        <div class="content">
-          <h3>Stage au CEA de Bruyère Le Châtel</h3>
-          <br>
-            <p>
-              Définition et implémentation d'un outil de recensement 
-              et de qualification des incidents du centre de calcul
-            </p>
-            <p>
-                Durant ce stage de 2 mois, j'ai eu l'opportunité de commencer la conception d'un outil 
-                de recensement et qualification des incidents sur les machines composant le supercalculateur 
-                afin que les équipes puissent communiquer les indisponibilitées prévues ou non sur ces machines.
-            </p>
-            <br>
-            <p>
-                L'application web a été conçue avec le framework Django dans un environnement de conteneurisation Docker.
-            </p>
+        <img rel="CEA" src="public/img/fast_concept.jpeg">
+        <a href="public/documents/STAGE1.pdf" style="text-decoration: none; color: white;">
+         <div class="content" >
+           <br> <br> <br> <br> <br> <br> <br> <br> <br> <br>
+           <h2 style="text-align: center; width: 38vw;">Stage 1</h2>
             <i style="display:none;">Cliquez ici pour voir mon rapport de stage</i>
-        </div>
+         </div>
         </a>
-      </div>
+     </div>  
+  
+    <div class="experience">
+        <img rel="CEA" src="public/img/fast_concept.jpeg">
+        <a href="#" target="_blank">
+         <div class="content">
+         <br> <br> <br> <br> <br> <br> <br> <br> <br> <br>
+           <h2 style="text-align: center;">Stage 2</h2>
+            <i style="display:none;">Cliquez ici pour voir mon rapport de stage</i>
+         </div>
+        </a>
+     </div>  
+  </div>
 
-    </div>
+
+
+
+
     <br> <br>
-    <h3>Pour voir mes différents projets dont mes projets de PPE, ils sont tous hébergé sur mon GitHub</h3>
+    <h3>Pour voir mes différents projets réaliser durant mon parcours scolaires, ils sont tous hébergé sur mon GitHub</h3>
     <a href="https://github.com/Simonbelhassen/" target="_blank"><button type="button" name="btnNext" class="btnNext"><img width="42" height="42" src="public/img/github.svg" alt="GitHub">Voir mon profil GitHub</button></a>
 
 </div>
